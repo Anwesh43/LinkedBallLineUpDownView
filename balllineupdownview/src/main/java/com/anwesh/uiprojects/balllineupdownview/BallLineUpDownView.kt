60ea9682bb5349a36cc0974fd9ba5829a19285c9
Anwesh43 <anweshthecool0@gmail.com>
2019-06-29 13:02:50 +0530

    adding BallLineUpDown class

--- balllineupdownview/src/main/java/com/anwesh/uiprojects/balllineupdownview/BallLineUpDownView.kt
+++ balllineupdownview/src/main/java/com/anwesh/uiprojects/balllineupdownview/BallLineUpDownView.kt
@@ -173,7 +173,31 @@ class BallLineUpDownView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class BallLineUpDown(var i : Int) {
+
+        private val root : BLUDNode = BLUDNode(0)
+        private var curr : BLUDNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
