42d0d021122577bdae4a192b5af9d4a60a26c5df
Anwesh43 <anweshthecool0@gmail.com>
2019-06-29 13:00:54 +0530

    adding BludNode class

--- balllineupdownview/src/main/java/com/anwesh/uiprojects/balllineupdownview/BallLineUpDownView.kt
+++ balllineupdownview/src/main/java/com/anwesh/uiprojects/balllineupdownview/BallLineUpDownView.kt
@@ -128,8 +128,52 @@ class BallLineUpDownView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class BLUDNode(var i : Int, val state : State = State()) {
+
+        private var next : BLUDNode? = null
+        private var prev : BLUDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BLUDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLUDNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLUDNode {
+            var curr : BLUDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
         }
     }
 }
