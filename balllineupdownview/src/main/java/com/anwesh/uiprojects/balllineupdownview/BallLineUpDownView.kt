e1c8ea602944b354b27129e288ca170acedc5848
Anwesh43 <anweshthecool0@gmail.com>
2019-06-29 12:54:38 +0530

    adding BallLineUpDownView class

--- balllineupdownview/src/main/java/com/anwesh/uiprojects/balllineupdownview/BallLineUpDownView.kt
+++ balllineupdownview/src/main/java/com/anwesh/uiprojects/balllineupdownview/BallLineUpDownView.kt
@@ -70,3 +70,19 @@ fun Canvas.drawBLUDNode(i : Int, scale : Float, paint : Paint) {
     drawBallsLineUp(sc1, size, paint)
     restore()
 }
+
+class BallLineUpDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
